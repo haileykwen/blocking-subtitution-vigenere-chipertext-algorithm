35b53aafabd3d8b268a7df722540fc8308fb7e1f
haileykwen <90519471+haileykwen@users.noreply.github.com>
2021-11-10 17:18:11 +0700

    Add subtitution algorithm

--- main.cpp
+++ main.cpp
@@ -43,9 +43,42 @@ int main()
         }
     }
     
-    cout << endl << "BLOCKING CIPHER TEXT = ";
+    cout << endl << "BLOCKING CHIPERTEXT= ";
     cout << blockingCipher << endl << endl;
     cout << "==========================";
+    
+    cout << endl << endl;
+    cout << "SUBTITUTING BLOCKING CHIPERTEXT";
+    cout << endl << endl;
+    
+    string subtitute;
+    int subtituteLength = blockingCipher.length();
+    
+    for (int m = 0; m < subtituteLength; m++) {
+        char target = blockingCipher[m];
+        int asci = int(target);
+        
+        // A - Z
+        if (asci >= 65 && asci <= 77) asci += 2;
+        if (asci >= 78 && asci <= 90) asci -= 2;
+        
+        // a - z
+        if (asci >= 97 && asci <= 109) asci += 2;
+        if (asci >= 110 && asci <= 122) asci -= 2;
+        
+        // 0 - 9
+        if (asci >= 48 && asci <= 52) asci += 2;
+        if (asci >= 53 && asci <= 57) asci -+ 2;
+        
+        // space & _
+        if (asci == 32) asci = 95;
+        if (asci == 95) asci = 32;
+        
+        char newAsci = char(asci);
+        subtitute += newAsci;
+    }
+    
+    cout << "SUBTITUTION CHIPERTEXT= " << subtitute << endl;
 
     return 0;
 }
